99f9fbf41b2c3f6c75587bd427b540c0f673fe6e
Neil Merton <neilmerton@gmail.com>
2023-12-05 17:33:24 +0000

    fix: :hammer: fingers crossed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://neilmerton.github.io',
   integrations: [mdx(), sitemap()],
+  site: 'https://neilmerton.github.io',
 });
