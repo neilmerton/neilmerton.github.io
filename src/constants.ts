147bb20ba81d87efaa058d7aa1db2b8805c58e24
Neil Merton <neilmerton@gmail.com>
2023-12-17 11:45:04 +0000

    feat: further tweaks to page layout

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export const ICON_PATHS = {
 
 export const SITE_TITLE = "Neil Merton";
 export const SITE_DESCRIPTION =
-  "A Frontend Developer based in Cheshire, UK, focused on accessibility and performance.";
+  "A Frontend Developer based in Cheshire, UK, focused on accessibility, performance and usability.";
 
 type ConceptItem = {
   label: string;
