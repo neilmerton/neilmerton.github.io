31547aaf670a9d6ff53982bbdafba495e4a8bc7d
Neil Merton <neilmerton@gmail.com>
2023-12-17 13:11:34 +0000

    feat: tweaks to uses content

--- src/constants.ts
+++ src/constants.ts
@@ -108,6 +108,11 @@ export const USES_HARDWARE_LIST: UsesItem[] = [
       "This replicates the Mac keyboard nicely and have no complaints about it. Maybe I'll try one of the more clicky ones next time.",
     title: "Logitech MX Key for Mac Keyboard",
   },
+  {
+    description:
+      "My first iPhone was the 3GS. After the iPhone 4 I switched over to Android. I've been back on iOS since the iPhone 8 Pro, want can't see myself going back to Android any time soon.",
+    title: "Apple iPhone 13 Pro",
+  },
 ];
 
 export type UsesItemWithHref = UsesItem & {
